4e87556b3569bd640b28a80a72441f00b636748c
bspriggs <ben@sprico.com>
2018-06-02 15:33:18 -0700

    Add tests for exit codes

--- tests/test_hpocket.sh
+++ tests/test_hpocket.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 test_hpocket_has_usage() {
+	assert ../hpocket.sh -h
+	assert ../hpocket.sh --help
 	assert_not_equals "" "$(../hpocket.sh -h)"
 	assert_not_equals "" "$(../hpocket.sh --help)"
 }
